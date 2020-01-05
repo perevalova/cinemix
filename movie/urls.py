11e4e9477c422fe1d6de07458f38a643a8731df2
Anastasia Perevalova <perev4n@gmail.com>
2020-01-05 20:29:59 +0200

    add filtering and lists of all movies

--- movie/urls.py
+++ movie/urls.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import MainPage, MovieDetail, MovieList, SearchView
+from .views import MainPage, MovieDetail, MovieList, SearchView, \
+    MovieCategoryList
 
 app_name = 'movie'
 urlpatterns = [
     path('', MainPage.as_view(), name='home'),
     path('search/', SearchView.as_view(), name='search'),
     # path('random/', Random.as_view(), name='random'),
-    path('<slug:type>/<slug:category>/', MovieList.as_view(), name='list'),
+    path('<slug:type>/', MovieList.as_view(), name='list'),
+    path('<slug:type>/<slug:category>/', MovieCategoryList.as_view(), name='list_category'),
     path('<slug:type>/<slug:category>/<slug:movie>/', MovieDetail.as_view(), name='detail'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.views.generic import DetailView, ListView
 from django.views.generic.base import View, TemplateView
 from star_ratings.models import Rating
 
-from movie.models import Movie, Category, MovieType
+from movie.models import Movie, Category, MovieType, Genre
 
 
 class MovieDetail(DetailView):
@@ -40,7 +40,14 @@ class MovieList(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        movies_list = Movie.objects.filter(type__slug=self.kwargs['type'], categories__slug=self.kwargs['category'])
+        movies_list = Movie.objects.filter(type__slug=self.kwargs['type'])
+        # genres = movies_list.
+
+        genres = self.request.GET.get('genres', '')
+        if genres:
+            for genre in genres:
+                movies_list = movies_list.filter(genres__slug=genre)
+
         order_by = self.request.GET.get('order_by', '')
         if order_by == 'newest':
             movies_list = movies_list.order_by('-year')
@@ -48,7 +55,8 @@ class MovieList(ListView):
             movies_list = movies_list.order_by('year')
 
         movie_type = self.kwargs['type']
-        movie_category = self.kwargs['category']
+
+        genres_list = Genre.objects.all()
 
 
         paginator = Paginator(movies_list, self.paginate_by)
@@ -64,6 +72,61 @@ class MovieList(ListView):
 
         context = {
             'movies': movies,
+            'genres': genres_list,
+            'type': movie_type,
+            # 'category': movie_category,
+        }
+
+        return context
+
+
+class MovieCategoryList(ListView):
+    template_name = 'movie_category_list.html'
+    model = Movie
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        movies_list = Movie.objects.filter(type__slug=self.kwargs['type'], categories__slug=self.kwargs['category'])
+
+        genres = self.request.GET.getlist('genres', '')
+
+        if genres:
+            for genre in genres:
+                movies_list = movies_list.filter(genres__slug=genre)
+
+        year_from = self.request.GET.get('year_from', '')
+        year_to = self.request.GET.get('year_to', '')
+
+        if year_from and year_to:
+            movies_list = movies_list.filter(year__range=(year_from, year_to))
+
+        order_by = self.request.GET.get('order_by', '')
+        if order_by == 'newest':
+            movies_list = movies_list.order_by('-year')
+        elif order_by == 'oldest':
+            movies_list = movies_list.order_by('year')
+
+        movie_type = self.kwargs['type']
+        movie_category = self.kwargs['category']
+
+        genres_list = Genre.objects.all()
+
+
+        paginator = Paginator(movies_list, self.paginate_by)
+
+        page = self.request.GET.get('page')
+
+        try:
+            movies = paginator.page(page)
+        except PageNotAnInteger:
+            movies = paginator.page(1)
+        except EmptyPage:
+            movies = paginator.page(paginator.num_pages)
+
+        context = {
+            'movies': movies,
+            'genres': genres_list,
             'type': movie_type,
             'category': movie_category,
         }
