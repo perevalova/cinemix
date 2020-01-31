07b584ee2a6999f407e3ccf3a51458c069cbb8f3
Anastasia Perevalova <perev4n@gmail.com>
2020-01-31 16:24:45 +0200

    add cache

--- cinemix/settings.py
+++ cinemix/settings.py
@@ -102,6 +102,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
