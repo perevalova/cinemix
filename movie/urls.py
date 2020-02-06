1038afae50cdbfe8f747ae5d7cdbe80928bd3955
Anastasia Perevalova <perev4n@gmail.com>
2020-02-06 11:10:11 +0200

    fix url collection's

--- movie/urls.py
+++ movie/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', MainPage.as_view(), name='home'),
     path('search/', SearchView.as_view(), name='search'),
     path('random/', RandomMovie.as_view(), name='random'),
-    path('<slug:collection>/', CollectionList.as_view(), name='collection'),
     path('<slug:type>/', MovieList.as_view(), name='list'),
+    path('collections/<slug:collection>/', CollectionList.as_view(), name='collection'),
     path('<slug:type>/<slug:category>/', MovieCategoryList.as_view(), name='list_category'),
     path('<slug:type>/<slug:category>/<slug:movie>/', MovieDetail.as_view(), name='detail'),
 ]
